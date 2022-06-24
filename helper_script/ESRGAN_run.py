5e27122581a7a91ea2b75390654d73132f4e7960
TsXor <74352334+TsXor@users.noreply.github.com>
2022-06-24 23:15:07 +0800

    Update ESRGAN_run.py

--- helper_script/ESRGAN_run.py
+++ helper_script/ESRGAN_run.py
@@ -59,26 +59,35 @@ else:
 model_dir = os.path.split(sys.argv[0])[0]+'\\models'
 models = os.listdir(model_dir)
 customize_model_usage = ""
+os.system('cls')
 if customize_model_usage.strip()=='':
   if len(models) == 0:
     print('models文件夹中没有模型，请放几个进去')
     input()
     exit()
+  for i in range(len(models)):
+    model = models[i]
+    print('\n\n[%d] %s' % (i+1, model))
+    with open('%s\%s\desc' % (model_dir,model)) as lines:
+      for line in lines:
+        print('    '+line, end='')
+  selected_model = int(input('\n\n请选择一个模型：'))
+  os.system('cls')
+  variants = os.listdir('%s\%s' % (model_dir,models[selected_model-1]))
+  variants.remove('desc')
+  if len(variants) == 1:
+    model_pth = '%s\%s\%s' % (model_dir,models[selected_model-1],variants[0])
   else:
-    for i in range(len(models)):
-      file = '%s\%s' % (model_dir,models[i])
-      if re.match('.*\.pth\.desc',file):
-        with open(file) as lines:
-          for line in lines:
-            print('    Description: %s' % line)
-      else:
-        print('[%d] %s' % (int(i/2+1), models[i]))
-  selected_model = input('请选择一个模型：')
-  model_pth = model_dir+'\\'+models[2*int(selected_model)-2]
+    for j in range(len(variants)):
+      variant = variants[j]
+      print('[%d] %s' % (j+1, variant))
+    selected_variant = int(input('请选择此模型的特定种类：'))
+    os.system('cls')
+    model_pth = '%s\%s\%s' % (model_dir,models[selected_model-1],variants[selected_variant-1])
   print('use model: '+model_pth)
 else:
   model_pth = customize_model_usage
-
+print('开始运行ESRGAN')
 upscale_exec = os.path.split(sys.argv[0])[0]+'\\ESRGAN-master\\upscale.py'
 tmp_bat = os.path.split(sys.argv[0])[0]+'\\tmp.bat'
 cmd_str = 'python %s --input %s --output %s%s%s --seamless %s%s%s%s%s%s --alpha-threshold %s --alpha-boundary-offset %s --alpha-mode %s%s %s' % (upscale_exec, input_folder, output_folder, skip_existing_output_files, delete_input_files_after_process, seamless, use_CPU_instead_of_CUDA, floating_point_16, cache_max_split_depth, binary_alpha, ternary_alpha, alpha_threshold, alpha_boundary_offset, alpha_mode, verbose, model_pth)
@@ -86,5 +95,4 @@ with open(tmp_bat,'w+') as cmd_file:
   cmd_file.write('@echo off && call conda activate ESRGAN 1>NUL 2>NUL && ' + cmd_str)
 os.system('%s && del %s' % (tmp_bat,tmp_bat))
 #cmd = f"python upscale.py --input '{input_folder}' --output '{output_folder}'{skip_existing_output_files}{delete_input_files_after_process} --seamless {seamless}{use_CPU_instead_of_CUDA}{floating_point_16}{cache_max_split_depth}{binary_alpha}{ternary_alpha} --alpha-threshold {alpha_threshold} --alpha-boundary-offset {alpha_boundary_offset} --alpha-mode {alpha_mode}{verbose} '{model_pth}'"
-print('ESRGAN process ended')
 input()
